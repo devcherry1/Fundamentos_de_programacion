445703fd0d2dc3d6b36ac08a6da3594c99c4adf0
HodginsOhannigan <62486573+devcherry1@users.noreply.github.com>
2024-11-04 15:01:04 -0500

    Paso 4 - Funciones y procedimientos

---
+++ Problema6/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Nombre: Juan Pablo Garcia Carrillo
+Grupo: 213022_727
+Programa: Ingenieria de sistemas
+Codigo fuente: Autoria Propia
+*/
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        //Aqui el usuario define la medida de cada lado del cuadrado
+        Console.WriteLine("Cuanto mide cada lado del cuadrado (en centimetros)?:");
+        int lado = validarNumero();
+
+        medidas(lado);
+
+        Console.WriteLine(); // Salto de línea al final
+    }
+    
+    // Funcion sin parametros para validar que la entrada sea un numero entero positivo
+    static int validarNumero()
+    {
+        int cantidad;
+        
+        while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
+        {
+            Console.Write("Por favor, ingresa un número entero positivo: ");
+        }
+        return cantidad;
+    }
+    
+    // Funcion con parametros sin retorno para mostrar area y perimetro del cuadrado
+    static void medidas(int lado)
+    {
+        int area = lado * lado;
+        int perimetro = lado * 4;
+        Console.WriteLine($"Teniendo en cuenta que cada lado de tu cuadrado mide {lado} centímetros:" +
+                          $"\nEl área del cuadrado es de {area} centimetros cuadrados." +
+                          $"\nMientras que el perímetro del cuadrado es de {perimetro} centímetros.");
+    }
+}
