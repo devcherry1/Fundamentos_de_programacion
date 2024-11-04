2c327598f7550dfa32d38600065fa3de62baa9b4
HodginsOhannigan <62486573+devcherry1@users.noreply.github.com>
2024-11-04 14:58:47 -0500

    Paso3

---
+++ Estructura_repetitiva/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Nombre: Juan Pablo Garcia Carrillo
+Grupo: 213022_727
+Programa: Ingenieria de sistemas
+Codigo fuente: Autoria Propia
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número entero positivo mayor que 1: ");
+        int cantidad;
+
+        // Validar que la entrada sea un numero entero positivo mayor que 1
+        while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 1)
+        {
+            Console.Write("Por favor, ingresa un número entero positivo mayor que 1: ");
+        }
+
+        int a = 1;
+
+        Console.WriteLine($"Estos son los primeros {cantidad} numeros de la serie Fibonacci:");
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write(a + "  ");
+            int siguiente = a + a;
+            a = siguiente;
+        }
+        Console.WriteLine(); // Salto de línea al final
+    }
+}
